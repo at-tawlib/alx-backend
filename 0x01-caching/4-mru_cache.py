dbd818f5f27764cebf7b61918d85fda09f154028
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-07-26 10:36:17 +0000

    Create 4-mru_cache.py

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+MRU caching
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRU caching system
+    """
+    cache_count: dict = {}
+
+    def __init__(self):
+        """
+        Initialization
+        used: stores used items, anytime item is called,
+        we check for it in the list, if found, delete it from
+        the current position then add it to the last position
+        """
+        super().__init__()
+        self.used = []
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        if number of items in cache is higher than BaseCaching.MAX_ITEMS,
+        discard the most recently used item and print it out then add
+        the new item
+        """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+        # if key is not in used items, add it else delete it
+        # from it's current position then append it to the list
+        if key not in self.used:
+            self.used.append(key)
+        else:
+            self.used.append(self.used.pop(self.used.index(key)))
+
+        if len(self.used) > BaseCaching.MAX_ITEMS:
+            # delete second to last item, since it is the most
+            # recently used before adding the last item
+            discard = self.used.pop(-2)
+            del self.cache_data[discard]
+            print('DISCARD: {}'.format(discard))
+
+    def get(self, key):
+        """Get an item by key"""
+        if key in self.cache_data.keys() and key is not None:
+            # remove item from the list and append it to the last postion
+            self.used.append(self.used.pop(self.used.index(key)))
+            return self.cache_data[key]
+        return None
