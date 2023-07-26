f7543fcbf6fa376d930de8ad725088ff9abc4b33
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-07-26 02:43:39 +0000

    Create 1-fifo_cache.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+FIFO caching
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFO caching system
+    """
+
+    def __init__(self):
+        """
+        Initialization
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        if number of items in cache is higher than BaseCaching.MAX_ITEMS,
+        discard the first item and print it out then add the new item
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            first_item = next(iter(self.cache_data))
+            self.cache_data.pop(first)
+            print('DISCARD: {}'.format(first_item))
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
